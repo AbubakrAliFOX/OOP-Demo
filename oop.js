d67762c35cdbdaab10f15dce51adc8e4d2f3a63f
Abubakr <thefox5000@gmail.com>
2023-11-13 14:36:09 +0300

    Inheritance

--- oop.js
+++ oop.js
@@ -6,15 +6,16 @@ class User {
     this.s = salary < 5000 ? salary + 500 : salary;
     User.count++;
   }
-  summary() {
-    return `Hello, ${this.n}. You salalry is ${this.s} - ID: ${this.i}`;
+  sayHi () {
+    return `Hello, ${this.n}`
   }
-  editName(newName) {
-    this.n = newName;
-  }
-  static sayHi() {
-    return `Hi`;
-  }
+}
+
+class Admin extends User {
+    constructor(id, name, salary, permissions) {
+        super(id, name, salary);
+        this.p = permissions;
+    }
 }
 
 const user1 = new User(1, "John", 2000);
@@ -22,10 +23,15 @@ const user2 = new User(2, "Jack", 2000);
 const user3 = new User(3, "Sam", 2000);
 const user4 = new User(4, "", 2000);
 
+////////////////////
+
+const adminMain = new Admin(5,'Ahmad', 4000, 'Access to all files');
+
 // console.log(user4.summary());
 // console.log(user4.editName("Jackyyy"));
 // console.log(user4.summary());
 // console.log(user4.n);
 
-console.log(User.count);
-console.log(User.sayHi());
+// console.log(User.count);
+
+console.log(adminMain.sayHi());
