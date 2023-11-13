4829f1d21141b7222ca25405e93e1da55c2e2264
Abubakr <thefox5000@gmail.com>
2023-11-13 13:59:35 +0300

    Add methods

--- oop.js
+++ oop.js
@@ -3,8 +3,11 @@
 class User {
     constructor(id, name, salary) {
         this.i = id;
-        this.n = name;
-        this.s = salary + 1002;
+        this.n = name || 'Unknown';
+        this.s = salary < 5000 ? salary + 500: salary;
+    }
+    summary () {
+        return `Hello, ${this.n}. You salalry is ${this.s} - ID: ${this.i}`
     }
 }
 
@@ -12,6 +15,7 @@ class User {
 const user1 = new User(1, 'John', 2000);
 const user2 = new User(2, 'Jack', 2000);
 const user3 = new User(3, 'Sam', 2000);
+const user4 = new User(4, '', 2000);
 
-console.log(user1.s, user2.s, user3.s);
+console.log(user4.summary());
 
