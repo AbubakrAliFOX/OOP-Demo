d3abc8535efef429f88f0982e1fcdd1091ac107a
Abubakr <thefox5000@gmail.com>
2023-11-18 08:24:06 +0300

    Worker example

--- oop.js
+++ oop.js
@@ -8,20 +8,19 @@ class User {
     this.#bankAccount = 123456;
     User.count++;
   }
-  sayHi () {
-    return `Hello, ${this.n}`
+  sayHi() {
+    return `Hello, ${this.n}`;
   }
-  printBankAccount () {
+  printBankAccount() {
     return this.#bankAccount;
   }
 }
 
 class Admin extends User {
-    constructor(id, name, salary, bankAccount , permissions) {
-        super(id, name, salary, bankAccount);
-        this.p = permissions;
-    }
-
+  constructor(id, name, salary, bankAccount, permissions) {
+    super(id, name, salary, bankAccount);
+    this.p = permissions;
+  }
 }
 
 const user1 = new User(1, "John", 2000);
@@ -31,7 +30,7 @@ const user4 = new User(4, "", 2000);
 
 ////////////////////
 
-const adminMain = new Admin(5,'Ahmad', 4000, 'Access to all files');
+const adminMain = new Admin(5, "Ahmad", 4000, "Access to all files");
 
 // console.log(user4.summary());
 // console.log(user4.editName("Jackyyy"));
@@ -40,7 +39,54 @@ const adminMain = new Admin(5,'Ahmad', 4000, 'Access to all files');
 
 // console.log(User.count);
 
-console.log(user1.printBankAccount());
-console.log(adminMain.printBankAccount());
+// console.log(user1.printBankAccount());
+// console.log(adminMain.printBankAccount());
 
-console.log(Admin.prototype.sayHi);
+// console.log(Admin.prototype.sayHi);
+
+
+// Task 1: Code a Person class
+class Person {
+  constructor(name = "Tom", age = 20, energy = 100) {
+      this.name = name;
+      this.age = age;
+      this.energy = energy;
+  }
+
+  sleep() {
+      this.energy += 10;
+  }
+
+  doSomethingFun() {
+      this.energy -= 10;
+  }
+}
+
+// Task 2: Code a Worker class
+class Worker extends Person {
+  constructor(name = "Tom", age = 20, energy = 100, xp = 0, hourlyWage = 10) {
+      super(name, age,energy)
+      this.xp = xp;
+      this.hourlyWage = hourlyWage;
+  }
+
+  goToWork() {
+      this.xp += 10;
+  }
+
+}
+// Task 3: Code an intern object, run methods
+function intern() {
+  const newWorker = new Worker(name= 'Bob', age= 21, energy= 110, xp= 0, hourlyWage= 10)
+  newWorker.goToWork();
+  return newWorker;
+}
+
+// Task 4: Code a manager object, methods
+function manager() {
+  newManagerdoSomethingFun()
+  return newManager;
+}
+const newManager = new Worker()
+
+console.log(intern());
